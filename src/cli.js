8d5bac3e11409142726ccf5a74f5b588b044829d
Vlad Kartashov <kartashov88890@yandex.ru>
2023-07-13 00:02:37 +0300

    upd1

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const name = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  return `Hello ${userName}!`;
+  const questionName = readlineSync.question('May I have your name? ');
+  const nameHello = `Hello, ${questionName}!`;
+  return nameHello;
 };
 export default name;
